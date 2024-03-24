e07e22d07ef59334e46cd3362a64b1afa5766f42
Kap Petrov <148530033+KapPetrov@users.noreply.github.com>
2024-03-24 01:47:52 -0700

    Assembler

---
+++ XISM-8/src/assembler/Assembler/Assembler/main.asm
@@ -0,0 +1,16 @@
+// Simple program
+// It outputs whatever was on the bus in a complicated way using all instructions
+// This is a test
+// This can be obviously optimized, no need for addition
+// nor subtraction.
+// Here's how it should be optimized:
+// lda
+// out
+// hlt
+
+lda
+add
+lda
+sub
+out
+hlt
